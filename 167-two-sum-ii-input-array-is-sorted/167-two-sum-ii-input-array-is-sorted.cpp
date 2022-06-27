da8b363bbd7defc8128ee253db3cf332fe6fd06e
suraj-narayan <76046501+suraj-narayan@users.noreply.github.com>
2022-06-27 15:15:52 +0530

    Time: 41 ms (5.22%), Space: 15.4 MB (92.76%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -6,17 +6,20 @@ public:
         int low=0, high=n-1;
         while(low<high)
         {
-            int sum=numbers[low]+numbers[high];
-            if(sum==target)
+            int l=numbers[low];
+            int h=numbers[high];
+            if(l+h==target)
             {
                 ans.push_back(low+1);
                 ans.push_back(high+1);
                 break;
             }
-            if(sum>target)
-                high--;
-            else
+            if(l+h<target)
                 low++;
+            if(l+h>target)
+                high--;
+            
+            
         }
         return ans;
     }
