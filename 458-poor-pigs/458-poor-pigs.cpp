b44ff2c4ea4c222100db190f1ba97f379b305bf1
suraj-narayan <76046501+suraj-narayan@users.noreply.github.com>
2022-08-07 21:30:08 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (80.73%) - LeetHub

---
+++ 458-poor-pigs/458-poor-pigs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    
+    int poorPigs(int b, int md, int mt) {
+        
+           
+                
+            int tests = mt/md + 1;
+        
+            return ceil(log(b)/log(tests));
+            
+        
+    }
+};
