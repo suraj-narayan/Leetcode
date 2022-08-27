f535db388550f5b30ba7457ab82e00becb2a9207
suraj-narayan <76046501+suraj-narayan@users.noreply.github.com>
2022-08-27 16:29:56 +0530

    Time: 1071 ms (33.93%), Space: 181.1 MB (47.84%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
@@ -3,45 +3,43 @@ public:
     
     
     
-    
-    int dfs(int u, int p, vector<vector<int>>& adj, vector<int>& restricted) {
-        if(binary_search(restricted.begin(), restricted.end(), u))
-            return 0;
-        int ans=1;
+   void tom(vector<vector<int>>& edges,unordered_map<int,int>& mp,int& ans,int i,int parent){
         
-        for(int i=0;i<adj[u].size();i++)
-        {
-            if(adj[u][i]==p)
-                continue;
-            ans+=dfs(adj[u][i], u, adj, restricted);
+        if(mp.find(i)!=mp.end()){
+            return;
         }
-        return ans;
+        ans++;
+      
+        for(int j=0;j<edges[i].size();j++){
+            if(edges[i][j]!=parent){
+                
+                tom(edges,mp,ans,edges[i][j],i);
+            }
+        }
+        return;
+        
     }
     
-    
-    
-    
-    
-    
     int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
-       
-        vector<vector<int>> adj(n);
-        for(auto i:edges)
-        {
-            adj[i[0]].push_back(i[1]);
-            adj[i[1]].push_back(i[0]);
+        
+        unordered_map<int,int> mp;
+        int i,k;
+        for(i=0;i<restricted.size();i++){
+            mp[restricted[i]]++;
         }
+        if(mp.find(0)!=mp.end()){
+            return 0;
+        }
+        vector<vector<int>> v(n);
+        for(i=0;i<edges.size();i++){
+            v[edges[i][0]].push_back(edges[i][1]);
+            v[edges[i][1]].push_back(edges[i][0]);
+        }
+        int ans=0;
+        tom(v,mp,ans,0,-1);
+        return ans;
         
-        
-        sort(restricted.begin(), restricted.end());
-        
-        return dfs(0, -1, adj, restricted);
-        
-        
-        
-        
-        
-        
+          
         
     }
 };
