6698a3987363033ef9dac651a32005542d3c890a
suraj-narayan <76046501+suraj-narayan@users.noreply.github.com>
2023-09-10 01:42:38 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (99.30%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
-    // Recursive function to count combinations
-    int f(int n, vector<int>& nums, int target, vector<int>& dp) {
-
-        // If the target is reached, return 1 (one valid combination found)
-        if (target == 0) {
+    int dp[1001];
+    
+    int util(vector<int>& nums, int n, int target) {
+        if(target==0)
             return 1;
-        }
-        // If the target becomes negative, it's not a valid combination
-        if (target < 0) {
+        if(target<0)
             return 0;
-        }
-        // If the result for the current target is already calculated, return it
-        if (dp[target] != -1) {
+        if(dp[target]!=-1)
             return dp[target];
+        int ans =0;
+        for(int i=0;i<n;i++) {
+            ans += util(nums, n, target-nums[i]);
         }
-        int take = 0;
-
-        // Iterate through the numbers and try taking each one to reach the target
-        for (int j = 0; j < n; ++j) {
-            take += f(n, nums, target - nums[j], dp); // Recursively count combinations
-        }
-
-        // Store the result in the DP table and return it
-        return dp[target] = take;
+        return dp[target]=ans;
     }
- 
+    
+    
     int combinationSum4(vector<int>& nums, int target) {
- 
-        vector<int>dp(target+1,-1);
-       return f(nums.size(),nums,target,dp);
-             
- 
+        int n=  nums.size();
+        memset(dp, -1, sizeof(dp));
+        return util(nums, n, target);
     }
 };
