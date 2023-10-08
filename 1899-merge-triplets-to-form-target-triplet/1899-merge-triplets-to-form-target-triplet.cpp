29a8985ec12221a42edcbdbb5c803acfd4bc1146
suraj-narayan <76046501+suraj-narayan@users.noreply.github.com>
2023-10-08 23:23:09 +0530

    Time: 395 ms (65.62%), Space: 147.8 MB (30.66%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
@@ -3,6 +3,8 @@ public:
     bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
         int ans = 0;
         for(auto i:triplets) {
+            if(ans==7)
+                return true;
             if( i[0]>target[0] or i[1]>target[1] or i[2]>target[2])
                 continue;
             for(int j =0; j<3;j++) {
