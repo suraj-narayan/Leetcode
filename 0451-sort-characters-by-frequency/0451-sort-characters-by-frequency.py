86e1ed2a3a680b43dd7d9790383bc4a0c5813887
suraj-narayan <76046501+suraj-narayan@users.noreply.github.com>
2024-02-07 17:22:39 +0530

    Time: 43 ms (77.40%), Space: 17.7 MB (93.07%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -0,0 +1,11 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        from collections import defaultdict
+        ump = defaultdict(int)
+        for c in s:
+            ump[c] += 1
+        str_list = [(value, key) for key, value in ump.items()]
+        str_list.sort(reverse = True)
+        str_values = [key*key2 for (key, key2) in str_list]
+        return "".join(str_values)
+        
